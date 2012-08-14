92c1bdc8dc61ca9bc0001b9100acec343b054d47
NorbertKocsis <norbert.kocsis@liferay.com>
2012-08-14 11:27:32 +0200

    Autogenerated

--- portal-service/src/com/liferay/portlet/asset/service/AssetLinkLocalService.java
+++ portal-service/src/com/liferay/portlet/asset/service/AssetLinkLocalService.java
@@ -418,4 +418,36 @@ public interface AssetLinkLocalService extends BaseLocalService,
 		int typeId)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
+
+	/**
+	* Returns all the IDs of the the asset links of the given link type whose
+	* first or second entry ID is the given entry ID.
+	*
+	* @param entryId the primary key of the asset entry
+	* @param typeId typeId the link type. Acceptable values include {@link
+	com.liferay.portlet.asset.model.AssetLinkConstants#TYPE_RELATED}
+	which is a bidirectional relationship and {@link
+	com.liferay.portlet.asset.model.AssetLinkConstants#TYPE_CHILD}
+	which is a unidirectional relationship. For more information see
+	{@link com.liferay.portlet.asset.model.AssetLinkConstants}
+	* @return the IDs of the asset links of the given link type whose first or
+	second entry ID is the given entry ID
+	* @throws SystemException if a system exception occurred
+	*/
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public long[] getLinkIds(long entryId, int typeId)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	/**
+	* Returns all the IDs of the asset links whose first or second entry ID is
+	* the given entry ID.
+	*
+	* @param entryId entryId the primary key of the asset entry
+	* @return the IDs of the asset links whose first or second entry ID is the
+	given entry ID
+	* @throws SystemException if a system exception occurred
+	*/
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public long[] getLinkIds(long entryId)
+		throws com.liferay.portal.kernel.exception.SystemException;
 }
