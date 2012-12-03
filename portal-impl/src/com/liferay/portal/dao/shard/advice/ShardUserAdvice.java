39b9b94216fc6d11fbb46db949b2b2afe2491a6a
Mate Thurzo <mate.thurzo@liferay.com>
2012-12-03 14:17:08 +0100

    LPS-30715 Source formatting and refactoring

--- portal-impl/src/com/liferay/portal/dao/shard/advice/ShardUserAdvice.java
+++ portal-impl/src/com/liferay/portal/dao/shard/advice/ShardUserAdvice.java
@@ -16,12 +16,19 @@ package com.liferay.portal.dao.shard.advice;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.model.User;
 
 import java.lang.reflect.Method;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+
+import org.apache.commons.lang.ArrayUtils;
 
 /**
  * @author Vilmos Papp
@@ -30,9 +37,12 @@ public class ShardUserAdvice implements MethodInterceptor {
 
 	public Object invoke(MethodInvocation methodInvocation) throws Throwable {
 		Method method = methodInvocation.getMethod();
+
 		String methodName = method.getName();
 
 		Object[] arguments = methodInvocation.getArguments();
+
+		Class<?>[] parameterTypes = method.getParameterTypes();
 
 		if ((arguments == null) || (arguments.length == 0)) {
 			return methodInvocation.proceed();
@@ -40,84 +50,47 @@ public class ShardUserAdvice implements MethodInterceptor {
 
 		Object argument1 = arguments[0];
 		Object argument2 = null;
+
 		if (arguments.length > 1) {
 			argument2 = arguments[1];
 		}
 
 		long companyId = -1;
 
-		if (argument1 instanceof Long) {
-			if (argument2 instanceof Long) {
-				if (methodName.equals("addUser") ||
-					methodName.equals("addUserWithWorkflow") ||
-					methodName.equals("getUserById") ||
-					methodName.equals("updateIncompleteUser")) {
+		try {
+			if (ArrayUtil.contains(parameterTypes, User.class)) {
+				int pos = ArrayUtils.indexOf(parameterTypes, User.class);
 
-					companyId = (Long)argument2;
-				}
-				else if (methodName.equals("authenticateByUserId")) {
-					companyId = (Long)argument1;
-				}
-			}
-			else if (methodName.equals("addDefaultAdminUser") ||
-					methodName.equals("authenticateByEmailAddress") ||
-					methodName.equals("authenticateByScreenName") ||
-					methodName.equals("authenticateForBasic") ||
-					methodName.equals("authenticateForDigest") ||
-					methodName.equals("checkLoginFailureByEmailAddress") ||
-					methodName.equals("checkLoginFailureByScreenName") ||
-					methodName.equals("decryptUserId") ||
-					methodName.equals("fetchUserByEmailAddress") ||
-					methodName.equals("fetchUserByScreenName") ||
-					methodName.equals("getCompanyUsers") ||
-					methodName.equals("getCompanyUsersCount") ||
-					methodName.equals("getDefaultUser") ||
-					methodName.equals("getDefaultUserId") ||
-					methodName.equals("getUserByEmailAddress") ||
-					methodName.equals("getUserByFacebookId") ||
-					methodName.equals("getUserByOpenId") ||
-					methodName.equals("getUserByScreenName") ||
-					methodName.equals("getUserIdByEmailAddress") ||
-					methodName.equals("getUserIdByScreenName") ||
-					methodName.equals("hasRoleUser") ||
-					methodName.equals("loadGetDefaultUser") ||
-					methodName.equals("search") ||
-					methodName.equals("searchCount") ||
-					methodName.equals("sendPassword") ||
-					methodName.equals("updateLockoutByEmailAddress") ||
-					methodName.equals("updateLockoutByScreenName")) {
+				User user = (User)arguments[pos];
 
-					companyId = (Long)argument1;
+				companyId = user.getCompanyId();
 			}
-		}
-		else if ((argument1 instanceof String) && (argument2 instanceof Long)) {
-			if (methodName.equals("getUserByUuidAndCompanyId")) {
+			else if (methodName.equals("addUser") &&
+					 !parameterTypes[0].equals(User.class)) {
+
+				companyId = (Long)argument2;
+			}
+			else if (methodName.equals("getUserById") &&
+					 Arrays.equals(parameterTypes, _TYPES_L_L)) {
+
+				companyId = (Long)argument2;
+			}
+			else if (methodName.equals("hasRoleUser") &&
+					 !Arrays.equals(parameterTypes, _TYPES_L_L)) {
+
+				companyId = (Long)argument1;
+			}
+			else if (_adviceMethodNamesArgument1.contains(methodName)) {
+				companyId = (Long)argument1;
+
+			}
+			else if (_adviceMethodNamesArgument2.contains(methodName)) {
 				companyId = (Long)argument2;
 			}
 		}
-		else if (argument1 instanceof User) {
-			if (methodName.equals("addUser") ||
-				methodName.equals("checkLockout") ||
-				methodName.equals("checkLoginFailure") ||
-				methodName.equals("checkPasswordExpired") ||
-				methodName.equals("completeUserRegistration") ||
-				methodName.equals("deleteUser") ||
-				methodName.equals("isPasswordExpired") ||
-				methodName.equals("isPasswordExpiringSoon") ||
-				methodName.equals("sendEmailAddressVerification") ||
-				methodName.equals("updateLockout") ||
-				methodName.equals("updateUser")) {
-
-				User user = (User)argument1;
-
-				companyId = user.getCompanyId();
-			}
-		}
-		else if (argument2 instanceof User) {
-			if (methodName.equals("updateAsset")) {
-				User user = (User)argument1;
-
-				companyId = user.getCompanyId();
+		catch (Exception e) {
+			if (_log.isErrorEnabled()) {
+				_log.error(e.getMessage());
 			}
 		}
 
@@ -125,15 +98,15 @@ public class ShardUserAdvice implements MethodInterceptor {
 			return methodInvocation.proceed();
 		}
 
-		if (_log.isInfoEnabled()) {
-			_log.info(
+		if (_log.isDebugEnabled()) {
+			_log.debug(
 				"Setting company service to shard of companyId " + companyId +
 					" for " + methodInvocation.toString());
 		}
 
-		Object returnValue = null;
-
 		_shardAdvice.pushCompanyService(companyId);
+
+		Object returnValue = null;
 
 		try {
 			returnValue = methodInvocation.proceed();
@@ -149,7 +122,54 @@ public class ShardUserAdvice implements MethodInterceptor {
 		_shardAdvice = shardAdvice;
 	}
 
+	private static final Class<?>[] _TYPES_L_L = {Long.TYPE, Long.TYPE};
+
 	private static Log _log = LogFactoryUtil.getLog(ShardUserAdvice.class);
+
+	private static Set<String> _adviceMethodNamesArgument1 =
+		new HashSet<String>();
+
+	private static Set<String> _adviceMethodNamesArgument2 =
+		new HashSet<String>();
+
+	static {
+
+		// Initialize method names for companyId in the 1st arg
+
+		_adviceMethodNamesArgument1.add("addDefaultAdminUser");
+		_adviceMethodNamesArgument1.add("authenticateByEmailAddress");
+		_adviceMethodNamesArgument1.add("authenticateByScreenName");
+		_adviceMethodNamesArgument1.add("authenticateByUserId");
+		_adviceMethodNamesArgument1.add("authenticateForBasic");
+		_adviceMethodNamesArgument1.add("authenticateForDigest");
+		_adviceMethodNamesArgument1.add("checkLoginFailureByEmailAddress");
+		_adviceMethodNamesArgument1.add("checkLoginFailureByScreenName");
+		_adviceMethodNamesArgument1.add("fetchUserByEmailAddress");
+		_adviceMethodNamesArgument1.add("fetchUserByScreenName");
+		_adviceMethodNamesArgument1.add("getUserByEmailAddress");
+		_adviceMethodNamesArgument1.add("getUserByFacebookId");
+		_adviceMethodNamesArgument1.add("getUserByOpenId");
+		_adviceMethodNamesArgument1.add("getUserByScreenName");
+		_adviceMethodNamesArgument1.add("getUserIdByEmailAddress");
+		_adviceMethodNamesArgument1.add("getUserIdByScreenName");
+		_adviceMethodNamesArgument1.add("getCompanyUsers");
+		_adviceMethodNamesArgument1.add("getCompanyUsersCount");
+		_adviceMethodNamesArgument1.add("getDefaultUser");
+		_adviceMethodNamesArgument1.add("getDefaultUserId");
+		_adviceMethodNamesArgument1.add("decryptUserId");
+		_adviceMethodNamesArgument1.add("loadGetDefaultUser");
+		_adviceMethodNamesArgument1.add("search");
+		_adviceMethodNamesArgument1.add("searchCount");
+		_adviceMethodNamesArgument1.add("sendPassword");
+		_adviceMethodNamesArgument1.add("updateLockoutByEmailAddress");
+		_adviceMethodNamesArgument1.add("updateLockoutByScreenName");
+
+		// Initialize method names for companyId in the 2nd arg
+
+		_adviceMethodNamesArgument2.add("getUserByUuidAndCompanyId");
+		_adviceMethodNamesArgument2.add("addUserWithWorkflow");
+		_adviceMethodNamesArgument2.add("updateIncompleteUser");
+	}
 
 	private ShardAdvice _shardAdvice;
 
