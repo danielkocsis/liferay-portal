c47b0bdc0f8b706620ba269de35381b8da826250
Vilmos Papp <vilmos.papp@liferay.com>
2012-12-03 14:21:32 +0100

    LPS-30715 Rebased

--- portal-impl/src/com/liferay/portal/dao/shard/advice/ShardUserAdvice.java
+++ portal-impl/src/com/liferay/portal/dao/shard/advice/ShardUserAdvice.java
@@ -31,6 +31,7 @@ import org.aopalliance.intercept.MethodInvocation;
 import org.apache.commons.lang.ArrayUtils;
 
 /**
+ * @author Máté Thurzó
  * @author Vilmos Papp
  */
 public class ShardUserAdvice implements MethodInterceptor {
