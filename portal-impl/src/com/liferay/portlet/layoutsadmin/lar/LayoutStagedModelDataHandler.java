ce787b2188aa12e9c2923a0d3213fccbcb032890
Daniel Kocsis <daniel.kocsis@liferay.com>
2014-04-29 10:15:52 +0200

    LPS-46232 Unrelated SF - Move it where it is used

--- portal-impl/src/com/liferay/portlet/layoutsadmin/lar/LayoutStagedModelDataHandler.java
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/lar/LayoutStagedModelDataHandler.java
@@ -44,7 +44,6 @@ import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.xml.Element;
-import com.liferay.portal.lar.LayoutExporter;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Image;
 import com.liferay.portal.model.Layout;
@@ -699,8 +698,7 @@ public class LayoutStagedModelDataHandler
 
 		typeSettings.setProperty(
 			"url",
-			url.substring(0, x) + LayoutExporter.SAME_GROUP_FRIENDLY_URL +
-				url.substring(y));
+			url.substring(0, x) + _SAME_GROUP_FRIENDLY_URL + url.substring(y));
 	}
 
 	protected void fixImportTypeSettings(Layout layout) throws Exception {
@@ -712,7 +710,7 @@ public class LayoutStagedModelDataHandler
 
 		String friendlyURL = (String)friendlyURLInfo[0];
 
-		if (!friendlyURL.equals(LayoutExporter.SAME_GROUP_FRIENDLY_URL)) {
+		if (!friendlyURL.equals(_SAME_GROUP_FRIENDLY_URL)) {
 			return;
 		}
 
@@ -1197,6 +1195,9 @@ public class LayoutStagedModelDataHandler
 			layout.setGroupId(groupId);
 		}
 	}
+
+	private static final String _SAME_GROUP_FRIENDLY_URL =
+		"/[$SAME_GROUP_FRIENDLY_URL$]";
 
 	private static Log _log = LogFactoryUtil.getLog(
 		LayoutStagedModelDataHandler.class);
