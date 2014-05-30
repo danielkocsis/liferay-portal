5f691a2d085476cf22e267c84dbf57f9de5d3e90
Daniel Kocsis <daniel.kocsis@liferay.com>
2014-05-30 09:10:26 +0200

    LPS-45350 Re-add missing context map parameter

--- portal-impl/src/com/liferay/portal/staging/StagingImpl.java
+++ portal-impl/src/com/liferay/portal/staging/StagingImpl.java
@@ -1336,6 +1336,7 @@ public class StagingImpl implements Staging {
 		Map<String, Serializable> taskContextMap =
 			new HashMap<String, Serializable>();
 
+		taskContextMap.put(Constants.CMD, Constants.PUBLISH);
 		taskContextMap.put(
 			"exportImportConfigurationId",
 			exportImportConfiguration.getExportImportConfigurationId());
@@ -1923,6 +1924,7 @@ public class StagingImpl implements Staging {
 		Map<String, Serializable> taskContextMap =
 			new HashMap<String, Serializable>();
 
+		taskContextMap.put(Constants.CMD, Constants.PUBLISH);
 		taskContextMap.put(
 			"exportImportConfigurationId",
 			exportImportConfiguration.getExportImportConfigurationId());
