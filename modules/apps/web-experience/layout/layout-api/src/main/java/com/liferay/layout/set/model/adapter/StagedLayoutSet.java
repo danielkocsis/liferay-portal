66b3bc52ed2c0c048df9363660df77d336eb2b53
Máté Thurzó <mate.thurzo@liferay.com>
2016-09-01 16:56:17 +0200

    LPS-000000 StagedLayoutSet should be a staged grouped model

--- modules/apps/web-experience/layout/layout-api/src/main/java/com/liferay/layout/set/model/adapter/StagedLayoutSet.java
+++ modules/apps/web-experience/layout/layout-api/src/main/java/com/liferay/layout/set/model/adapter/StagedLayoutSet.java
@@ -16,6 +16,7 @@ package com.liferay.layout.set.model.adapter;
 
 import com.liferay.exportimport.kernel.lar.StagedModelType;
 import com.liferay.portal.kernel.model.LayoutSet;
+import com.liferay.portal.kernel.model.StagedGroupedModel;
 import com.liferay.portal.kernel.model.StagedModel;
 
 import java.util.Optional;
@@ -23,7 +24,7 @@ import java.util.Optional;
 /**
  * @author Mate Thurzo
  */
-public interface StagedLayoutSet extends LayoutSet, StagedModel {
+public interface StagedLayoutSet extends LayoutSet, StagedGroupedModel {
 
 	public Optional<String> getLayoutSetPrototypeName();
 
