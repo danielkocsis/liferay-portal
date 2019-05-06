c70f9b6a209e73e74bf96badf0d67ca6f1a8d208
Zoltán Császi <zoltan.csaszi@liferay.com>
2019-05-06 09:41:08 +0200

    LPS-92517 inherited group locales modified to available

---
+++ portal-impl/src/com/liferay/portal/upgrade/v7_2_x/UpgradeGroup.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v7_2_x;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Zoltan Csaszi
+ */
+public class UpgradeGroup extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		updateLanguageSettings();
+	}
+
+	protected void updateLanguageSettings() throws Exception {
+		try (PreparedStatement selectStatement = connection.prepareStatement(
+				"select groupId, typeSettings from Group_")) {
+
+			try (ResultSet rs = selectStatement.executeQuery()) {
+				while (rs.next()) {
+					String typeSettings = rs.getString("typeSettings");
+
+					UnicodeProperties typeSettingsProperties =
+						new UnicodeProperties(true);
+
+					typeSettingsProperties.load(typeSettings);
+
+					if (!GetterUtil.getBoolean(
+							typeSettingsProperties.getProperty(
+								"inheritLocales"))) {
+
+						continue;
+					}
+
+					long groupId = rs.getLong("groupId");
+
+					String[] languageIdsArray = StringUtil.split(
+						typeSettingsProperties.getProperty("locales"));
+
+					List<String> languageIds = new ArrayList<>();
+
+					for (String languageId : languageIdsArray) {
+						if (!LanguageUtil.isAvailableLocale(
+								LocaleUtil.fromLanguageId(languageId))) {
+
+							languageIds.add(languageId);
+						}
+					}
+
+					typeSettingsProperties.put(
+						"locales", StringUtil.merge(languageIds));
+
+					try (PreparedStatement updateStatement =
+							connection.prepareStatement(
+								"update Group_ set typeSettings = ? where " +
+									"groupId = ?")) {
+
+						updateStatement.setString(
+							1, typeSettingsProperties.toString());
+						updateStatement.setLong(2, groupId);
+
+						updateStatement.executeUpdate();
+					}
+				}
+			}
+		}
+	}
+
+}
